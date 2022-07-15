d12cf4a65f4d0bb6228c48465c20c915dc54b333
Renzo Pigliacampo <renzopigliacampo@gmail.com>
2022-07-15 19:41:37 -0300

    Code cleanup (SVGs, spacing, semicolons, single/double quotes)

--- assets/scripts/general.js
+++ assets/scripts/general.js
@@ -1,57 +1,57 @@
 $(() => {
-    // Wait for everything to load before displaying.
-    $(window).ready(() => {
-        $("body").addClass("smooth");
-        setTransitionTime(1);
-    });
+  // Wait for everything to load before displaying.
+  $(window).ready(() => {
+    $('body').addClass('smooth');
+    setTransitionTime(1);
+  });
 
-    // Open/close sidenav.
-    $("div#menu-open").on('click', () => {
-        $("#navbar-open").toggleClass('navbar-open');
-        $("#navbar-overlay").toggleClass('navbar-open');
-    });
-    $("div#navbar-overlay").on('click', () => {
-        $("#navbar-open").toggleClass('navbar-open');
-        $("#navbar-overlay").toggleClass('navbar-open');
-    });
+  // Open/close sidenav.
+  $('div#menu-open').on('click', () => {
+    $('#navbar-open').toggleClass('navbar-open');
+    $('#navbar-overlay').toggleClass('navbar-open');
+  });
+  $('div#navbar-overlay').on('click', () => {
+    $('#navbar-open').toggleClass('navbar-open');
+    $('#navbar-overlay').toggleClass('navbar-open');
+  });
 
-    // Switch between light and dark theme.
-    $("div#dark-mode").on('click', () => { switchTheme(); });
-    // Check the local storage for theming.
-    if (localStorage.getItem("theme") === "dark") {
-        if (!document.body.classList.contains('dark-mode')) {
-            switchTheme();
-        }
+  // Switch between light and dark theme.
+  $('div#dark-mode').on('click', () => { switchTheme(); });
+  // Check the local storage for theming.
+  if (localStorage.getItem('theme') === 'dark') {
+    if (!document.body.classList.contains('dark-mode')) {
+      switchTheme();
     }
+  }
 
-    // Set timer for short hovers and smooth transitions.
-    const root = document.documentElement;
-    const setTransitionTime = (timeout) => {
-        if (!timeout) { root.style.setProperty('--time', '0.5s ease-in-out'); }
-        else { setTimeout(() => { root.style.setProperty('--time', '0.2s'); }, 600); }
-    }
+  // Set timer for short hovers and smooth transitions.
+  const root = document.documentElement;
+  const setTransitionTime = (timeout) => {
+    if (!timeout) { root.style.setProperty('--time', '0.5s ease-in-out'); }
+    else { setTimeout(() => { root.style.setProperty('--time', '0.2s'); }, 600); }
+  }
 
-    const switchTheme = () => {
-        setTransitionTime(0);
-        let themeSwitch = document.body.classList.toggle('dark-mode');
-        // Store on the client's local storage for persistency.
-        localStorage.setItem("theme", themeSwitch ? "dark" : "light");
-        // Update dark mode button.
-        updateTheme();
-        setTransitionTime(1);
-    }
-
-    const updateTheme = () => {
-        let themeSwitch = document.body.classList.contains('dark-mode');
-        // Update dark mode button.
-        $("#dark-icon-header").css("-webkit-mask-image", themeSwitch
-            ? "url(/assets/images/icons/icon-sunny.svg)"
-            : "url(/assets/images/icons/icon-moon.svg)");
-        $("#dark-icon").css("-webkit-mask-image", themeSwitch
-            ? "url(/assets/images/icons/icon-sunny.svg)"
-            : "url(/assets/images/icons/icon-moon.svg)");
-        $("#dark-text").text(themeSwitch ? "Light Mode" : "Dark Mode");
-    }
+  const switchTheme = () => {
+    setTransitionTime(0);
+    let themeSwitch = document.body.classList.toggle('dark-mode');
+    // Store on the client's local storage for persistency.
+    localStorage.setItem('theme', themeSwitch ? 'dark' : 'light');
     // Update dark mode button.
     updateTheme();
+    setTransitionTime(1);
+  }
+
+  const updateTheme = () => {
+    let themeSwitch = document.body.classList.contains('dark-mode');
+    // Update dark mode button.
+    $('#dark-icon-header').css('-webkit-mask-image', themeSwitch
+      ? 'url(/assets/images/icons/icon-sunny.svg)'
+      : 'url(/assets/images/icons/icon-moon.svg)');
+    $('#dark-icon').css('-webkit-mask-image', themeSwitch
+      ? 'url(/assets/images/icons/icon-sunny.svg)'
+      : 'url(/assets/images/icons/icon-moon.svg)');
+    $('#dark-text').text(themeSwitch ? 'Light Mode' : 'Dark Mode');
+  }
+  // Update dark mode button.
+  updateTheme();
 });
--- assets/scripts/post.js
+++ assets/scripts/post.js
@@ -1,30 +1,30 @@
 $(() => {
-    // Get all hidden collapsible buttons.
-    let collapseHide = document.getElementsByClassName("collapsible-hide");
-    for (let i = 0; i < collapseHide.length; i++) {
-        // Attach a click listener to all buttons.
-        collapseHide[i].addEventListener("click", () => {
-            // Select the box element and switch the visibility.
-            let content = document.getElementById(this.id + "-data");
-            content.style.display = content.style.display === "block" ? "none" : "block";
-            // Toggle class to change the button color and text.
-            content.classList.toggle("active");
-            this.innerHTML = this.classList.toggle("active") ? "Press to hide the code" : "Press to show the code";
-            // This affects the element style, so it breaks its responsivity once pressed.
-        });
-    }
-    // Get all shown collapsible buttons.
-    let collapseShow = document.getElementsByClassName("collapsible-show");
-    for (let i = 0; i < collapseShow.length; i++) {
-        // Attach a click listener to all buttons.
-        collapseShow[i].addEventListener("click", () => {
-            // Select the box element and switch the visibility.
-            let content = document.getElementById(this.id + "-data");
-            content.style.display = content.style.display === "block" ? "none" : "block";
-            // Toggle class to change the button color and text.
-            content.classList.toggle("active");
-            this.classList.toggle("active");
-            // This affects the element style, so it breaks its responsivity once pressed.
-        });
-    }
+  // Get all hidden collapsible buttons.
+  let collapseHide = document.getElementsByClassName('collapsible-hide');
+  for (let i = 0; i < collapseHide.length; i++) {
+    // Attach a click listener to all buttons.
+    collapseHide[i].addEventListener('click', () => {
+      // Select the box element and switch the visibility.
+      let content = document.getElementById(this.id + '-data');
+      content.style.display = content.style.display === 'block' ? 'none' : 'block';
+      // Toggle class to change the button color and text.
+      content.classList.toggle('active');
+      this.innerHTML = this.classList.toggle('active') ? 'Press to hide the code' : 'Press to show the code';
+      // This affects the element style, so it breaks its responsivity once pressed.
+    });
+  }
+  // Get all shown collapsible buttons.
+  let collapseShow = document.getElementsByClassName('collapsible-show');
+  for (let i = 0; i < collapseShow.length; i++) {
+    // Attach a click listener to all buttons.
+    collapseShow[i].addEventListener('click', () => {
+      // Select the box element and switch the visibility.
+      let content = document.getElementById(this.id + '-data');
+      content.style.display = content.style.display === 'block' ? 'none' : 'block';
+      // Toggle class to change the button color and text.
+      content.classList.toggle('active');
+      this.classList.toggle('active');
+      // This affects the element style, so it breaks its responsivity once pressed.
+    });
+  }
 });
